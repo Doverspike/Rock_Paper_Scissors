58e515f9eb2470b28890e99d620a7f4cd1779eb2
Doverspike <derondoverspike@gmail.com>
2024-03-12 22:19:54 -0400

    Created function for playRound

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -2,13 +2,44 @@
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 300) + 3;
     if (randomNumber <= 100) {
-        return "Rock";
+        return "rock";
     }
     else if (randomNumber <= 200) {
-        return "Paper";
+        return "paper";
     }
     else if (randomNumber >= 201) {
-        return "Scissors";
+        return "scissors";
     }
 }
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You lose! Paper covers Rock.";
+        } else if (computerSelection === "scissors") {
+            return "You win! Rock smashes Scissors.";
+        } else if (computerSelection === "rock") {
+            return "You tie! You both picked Rock."
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You win! Paper covers Rock.";
+        } else if (computerSelection === "paper") {
+            return "You tie! You both picked Paper.";
+        } else if (computerSelection === "scissors") {
+            return "You lose! Scissors cuts Paper.";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            return "You lose! Rock crushes Scissors.";
+        } else if (computerSelection === "paper") {
+            return "You win! Scissors cuts paper.";
+        } else if (computerSelection === "scissors") {
+            return "You tie! You both picked Scissors.";
+        }
+    }
 
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
